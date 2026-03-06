6f277ce6f74904aeaba63ad24f1d29affe4321d1
Bernhard Werth <bernhard.werth@fh-hagenberg.at>
2026-03-06 07:18:17 +0100

    add meta opt

---
+++ src/HeuristicLib.Extensions/Problems/MetaOptimization/CompositeSearchSpace.cs
@@ -0,0 +1,114 @@
+﻿using HEAL.HeuristicLib.Execution;
+using HEAL.HeuristicLib.Operators;
+using HEAL.HeuristicLib.Operators.Creators;
+using HEAL.HeuristicLib.Operators.Crossovers;
+using HEAL.HeuristicLib.Operators.Mutators;
+using HEAL.HeuristicLib.Optimization;
+using HEAL.HeuristicLib.Random;
+using HEAL.HeuristicLib.SearchSpaces;
+
+namespace HEAL.HeuristicLib.Problems.MetaOptimization;
+
+public static class CompositeSearchSpace
+{
+  public static CompositeSearchSpace<T1, TS1, T2, TS2> WithSearchSpace<T1, TS1, T2, TS2>(this TS1 enc1, TS2 enc2)
+    where T1 : class
+    where TS1 : class, ISearchSpace<T1>
+    where T2 : class
+    where TS2 : class, ISearchSpace<T2>
+    => new(enc1, enc2);
+}
+
+public record CompositeSearchSpace<T1, TS1, T2, TS2>(TS1 SearchSpace, TS2 SearchSpace2) : ISearchSpace<CompositeGenotype<T1, T2>>
+  where T1 : class
+  where T2 : class
+  where TS1 : class, ISearchSpace<T1>
+  where TS2 : class, ISearchSpace<T2>
+{
+  public readonly NoProblem<T1, TS1> NoProblem1 = new(SearchSpace);
+  public readonly NoProblem<T2, TS2> NoProblem2 = new(SearchSpace2);
+  public bool Contains(CompositeGenotype<T1, T2> genotype) => SearchSpace.Contains(genotype.Part1) && SearchSpace2.Contains(genotype.Part2);
+
+  public Creator CombineCreators(ICreator<T1, TS1, IProblem<T1, TS1>> operator1, ICreator<T2, TS2, IProblem<T2, TS2>> operator2) => new(operator1, operator2);
+  public Mutator CombineMutator(IMutator<T1, TS1, IProblem<T1, TS1>> operator1, IMutator<T2, TS2, IProblem<T2, TS2>> operator2) => new(operator1, operator2);
+  public Crossover CombineCrossover(ICrossover<T1, TS1, IProblem<T1, TS1>> operator1, ICrossover<T2, TS2, IProblem<T2, TS2>> operator2) => new(operator1, operator2);
+
+  public record Creator(ICreator<T1, TS1, IProblem<T1, TS1>> Operator1, ICreator<T2, TS2, IProblem<T2, TS2>> Operator2)
+    : Creator<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>
+  {
+    public override Instance CreateExecutionInstance(ExecutionInstanceRegistry instanceRegistry)
+      => new(instanceRegistry.Resolve(Operator1), instanceRegistry.Resolve(Operator2));
+
+    public new sealed class Instance(ICreatorInstance<T1, TS1, IProblem<T1, TS1>> creator1Instance, ICreatorInstance<T2, TS2, IProblem<T2, TS2>> creator2Instance)
+      : Creator<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>.Instance
+    {
+      public override IReadOnlyList<CompositeGenotype<T1, T2>> Create(int count, IRandomNumberGenerator random, CompositeSearchSpace<T1, TS1, T2, TS2> searchSpace)
+      {
+        var parts1 = creator1Instance.Create(count, random, searchSpace.SearchSpace, searchSpace.NoProblem1);
+        var parts2 = creator2Instance.Create(count, random, searchSpace.SearchSpace2, searchSpace.NoProblem2);
+        return parts1.Zip(parts2, (a, b) => new CompositeGenotype<T1, T2>(a, b)).ToList();
+      }
+    }
+  }
+
+  public record Crossover(ICrossover<T1, TS1, IProblem<T1, TS1>> Operator1, ICrossover<T2, TS2, IProblem<T2, TS2>> Operator2)
+    : Crossover<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>
+  {
+    public override Instance CreateExecutionInstance(ExecutionInstanceRegistry instanceRegistry)
+      => new(instanceRegistry.Resolve(Operator1), instanceRegistry.Resolve(Operator2));
+
+    public new sealed class Instance(ICrossoverInstance<T1, TS1, IProblem<T1, TS1>> instance1, ICrossoverInstance<T2, TS2, IProblem<T2, TS2>> instance2)
+      : Crossover<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>.Instance
+    {
+      public override IReadOnlyList<CompositeGenotype<T1, T2>> Cross(IReadOnlyList<IParents<CompositeGenotype<T1, T2>>> parents, IRandomNumberGenerator random, CompositeSearchSpace<T1, TS1, T2, TS2> searchSpace)
+      {
+        var res1 = instance1.Cross(parents.Select(Selector1).ToArray(), random, searchSpace.SearchSpace, searchSpace.NoProblem1);
+        var res2 = instance2.Cross(parents.Select(Selector2).ToArray(), random, searchSpace.SearchSpace2, searchSpace.NoProblem2);
+        return res1.Zip(res2, ((a, b) => new CompositeGenotype<T1, T2>(a, b))).ToArray();
+
+        static IParents<T2> Selector2(IParents<CompositeGenotype<T1, T2>> x) => new Parents<T2>(x.Parent1.Part2, x.Parent2.Part2);
+        static IParents<T1> Selector1(IParents<CompositeGenotype<T1, T2>> x) => new Parents<T1>(x.Parent1.Part1, x.Parent2.Part1);
+      }
+    }
+  }
+
+  public record Mutator(IMutator<T1, TS1, IProblem<T1, TS1>> Operator1, IMutator<T2, TS2, IProblem<T2, TS2>> Operator2)
+    : Mutator<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>
+  {
+    public override Instance CreateExecutionInstance(ExecutionInstanceRegistry instanceRegistry)
+      => new(this, instanceRegistry.Resolve(Operator1), instanceRegistry.Resolve(Operator2));
+
+    public bool All { get; init; } = true;
+
+    public new sealed class Instance(Mutator mutator, IMutatorInstance<T1, TS1, IProblem<T1, TS1>> instance1, IMutatorInstance<T2, TS2, IProblem<T2, TS2>> instance2)
+      : Mutator<CompositeGenotype<T1, T2>, CompositeSearchSpace<T1, TS1, T2, TS2>>.Instance
+    {
+      public override IReadOnlyList<CompositeGenotype<T1, T2>> Mutate(IReadOnlyList<CompositeGenotype<T1, T2>> parents, IRandomNumberGenerator random, CompositeSearchSpace<T1, TS1, T2, TS2> searchSpace)
+      {
+        if (mutator.All) {
+          var res1 = instance1.Mutate(parents.Select(x => x.Part1).ToArray(), random, searchSpace.SearchSpace, searchSpace.NoProblem1);
+          var res2 = instance2.Mutate(parents.Select(x => x.Part2).ToArray(), random, searchSpace.SearchSpace2, searchSpace.NoProblem2);
+          return res1.Zip(res2, ((a, b) => new CompositeGenotype<T1, T2>(a, b))).ToArray();
+        }
+
+        //choose one mutator
+        var b = parents.Select(_ => random.NextInt(0, 1, true)).ToArray();
+        var res = parents.ToArray();
+
+        var p1 = parents.Select((p, i) => (p, i)).Where(t => b[t.i] == 0).Select(t => t.i).ToArray();
+        var mutants1 = instance1.Mutate(p1.Select(t => parents[t].Part1).ToArray(), random, searchSpace.SearchSpace, searchSpace.NoProblem1);
+        foreach (var (i, p) in p1.Zip(mutants1)) {
+          res[i] = res[i] with { Part1 = p };
+        }
+
+        var p2 = parents.Select((p, i) => (p, i)).Where(t => b[t.i] == 1).Select(t => t.i).ToArray();
+        var mutants2 = instance2.Mutate(p1.Select(t => parents[t].Part2).ToArray(), random, searchSpace.SearchSpace2, searchSpace.NoProblem2);
+        foreach (var (i, p) in p2.Zip(mutants2)) {
+          res[i] = res[i] with { Part2 = p };
+        }
+
+        return res;
+      }
+    }
+  }
+}
---
+++ test/HeuristicLib.Extensions.Tests/MetaOptimization/MetaOptimizationTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using HEAL.HeuristicLib.Algorithms;
+using HEAL.HeuristicLib.Algorithms.Evolutionary;
+using HEAL.HeuristicLib.Algorithms.LocalSearch;
+using HEAL.HeuristicLib.Algorithms.MetaAlgorithms;
+using HEAL.HeuristicLib.Genotypes.Vectors;
+using HEAL.HeuristicLib.Operators;
+using HEAL.HeuristicLib.Operators.Creators.RealVectorCreators;
+using HEAL.HeuristicLib.Operators.Crossovers.RealVectorCrossovers;
+using HEAL.HeuristicLib.Operators.Evaluators;
+using HEAL.HeuristicLib.Operators.Mutators;
+using HEAL.HeuristicLib.Operators.Mutators.IntegerVectorMutators;
+using HEAL.HeuristicLib.Operators.Mutators.RealVectorMutators;
+using HEAL.HeuristicLib.Operators.Replacers;
+using HEAL.HeuristicLib.Operators.Selectors;
+using HEAL.HeuristicLib.Optimization;
+using HEAL.HeuristicLib.Problems;
+using HEAL.HeuristicLib.Problems.MetaOptimization;
+using HEAL.HeuristicLib.Problems.TestFunctions;
+using HEAL.HeuristicLib.Problems.TestFunctions.SingleObjectives;
+using HEAL.HeuristicLib.Random;
+using HEAL.HeuristicLib.SearchSpaces.Vectors;
+
+namespace HEAL.HeuristicLib.Extensions.Tests.MetaOptimization;
+
+public class MetaOptimizationTests
+{
+  [Fact]
+  public void TestGAwithMutators()
+  {
+    var e1 = new CompositeGenotype<RealVector, IntegerVector>(new RealVector(), new IntegerVector(1));
+    var e2 = new CompositeGenotype<RealVector, IntegerVector>(new RealVector(), new IntegerVector(1));
+    Assert.Equal(e1, e2);
+    //setup
+    var problem = new TestFunctionProblem(new AckleyFunction(20));
+    var ga = GeneticAlgorithm.Create(
+      new UniformDistributedCreator(),
+      new SimulatedBinaryCrossover(),
+      new GaussianMutator(0.5, 0.5), 0.25,
+      new TournamentSelector<RealVector>(2),
+      new ElitismReplacer<RealVector>(1),
+      100,
+      new DirectEvaluator<RealVector>(),
+      null);
+
+    //test these mutators
+    //TODO c# can not infer array type 
+    var e = new StatelessMutator<RealVector, RealVectorSearchSpace>[] {
+      new GaussianMutator(0.5, 0.5),
+      new GaussianMutator(0.5, 1),
+      new PolynomialMutator(),
+      new PolynomialMutator(atLeastOnce: true)
+    };
+
+    var log = new ConcurrentBag<string>();
+
+    //build meta problem
+    var b = new MetaOptimizationProblemExamples.MetaOptimzationSearchSpaceBuilder();
+    var mutatorExtractor = b.AddChoiceParameter(e);
+    var metaSpace = b.Build();
+    var metaProblem = problem.AsMetaProblem(metaSpace, x => {
+      var alg = ga with { Mutator = mutatorExtractor(x) };
+      log.Add($"new try {x.Part2[0]}");
+      return alg.WithMaxIterations(1000 / alg.PopulationSize);
+    });
+
+    //build meta alg
+    var hc = HillClimber.GetBuilder(
+      creator: metaSpace.CombineCreators(
+        new UniformDistributedCreator(),
+        new Operators.Creators.IntegerVectorCreators.UniformDistributedCreator()), //operator name clash ...
+      mutator: metaSpace.CombineMutator(
+        new PolynomialMutator(),
+        new UniformOnePositionManipulator()));
+    hc.BatchSize = 4;
+    hc.Evaluator = hc.Evaluator
+                     .AsRepeated(11, objectives => objectives.Median(problem.Objective))
+                     .WithCache();
+
+    //run alg
+    hc.Build()
+      .WithMaxIterations(5)
+      .RunToCompletion(metaProblem, RandomNumberGenerator.Create(42), ct: CancellationToken.None);
+  }
+}
